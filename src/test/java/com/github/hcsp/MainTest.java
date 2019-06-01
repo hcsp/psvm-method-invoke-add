4743ebcc93d013cfaade3edcf717371c9d3aaf7e
Bo Zhang <bo@gradle.com>
2019-06-01 17:18:31 +0800

    Use CaptureSystemOutputExtension

--- src/test/java/com/github/hcsp/MainTest.java
+++ src/test/java/com/github/hcsp/MainTest.java
@@ -1,22 +1,18 @@
 package com.github.hcsp;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.hamcrest.Matchers.containsStringIgnoringCase;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import com.github.blindpirate.extensions.CaptureSystemOutput;
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
-    public void importStringUtilsCorrectly() {
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(baos, true));
-
+    @CaptureSystemOutput
+    public void importStringUtilsCorrectly(CaptureSystemOutput.OutputCapture capture) {
+        capture.expect(containsStringIgnoringCase("1+1=2"));
+        capture.expect(containsStringIgnoringCase("2+1=3"));
+        capture.expect(containsStringIgnoringCase("3+1=4"));
         Main.main(null);
-
-        assertTrue(new String(baos.toByteArray()).contains("1+1=2"));
-        assertTrue(new String(baos.toByteArray()).contains("2+1=3"));
-        assertTrue(new String(baos.toByteArray()).contains("3+1=4"));
     }
 
     @Test
