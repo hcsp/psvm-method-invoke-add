6357ceef56cc1f3fdbf5f242223dc8d6f9db16d9
wangyujie1207 <82414199@163.com>
2019-10-09 23:21:45 +0800

    complete addOne method (#97)
    complete addOne method (#97)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,13 +1,15 @@
 package com.github.hcsp;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("1+1=" + addOne(1));
-        System.out.println("2+1=" + addOne(2));
-        System.out.println("3+1=" + addOne(3));
-    }
+  public static void main(String[] args) {
+    System.out.println("1+1=" + addOne(1));
+    System.out.println("2+1=" + addOne(2));
+    System.out.println("3+1=" + addOne(3));
+  }
 
-    // Create an addOne(int i) method here to increment an integer
-    // 在这里编写一个addOne(int i)方法，对整数执行加1操作
-
+  // Create an addOne(int i) method here to increment an integer
+  // 在这里编写一个addOne(int i)方法，对整数执行加1操作
+  public static int addOne(int x) {
+    return x + 1;
+  }
 }
