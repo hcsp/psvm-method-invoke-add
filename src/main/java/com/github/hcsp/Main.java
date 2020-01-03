3bc0d01a4600eaa20bb53afc6bb4cfaa3d90d806
zml59 <wan624711573@163.com>
2020-01-03 23:44:43 +0800

    添加方法addOne，使输出值比输入值增加1。

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -2,12 +2,14 @@ package com.github.hcsp;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1+1=" + addOne(1));
-        System.out.println("2+1=" + addOne(2));
-        System.out.println("3+1=" + addOne(3));
+        System.out.println("1+1=" + addOne());
+        System.out.println("2+1=" + addOne());
+        System.out.println("3+1=" + addOne());
     }
-
     // Create an addOne(int i) method here to increment an integer
-    // 在这里编写一个addOne(int i)方法，对整数执行加1操作
-
+    // 在这里编写一个addOne(int i)方法，对整数执行加1i操作
+    public static int addOne(){
+        int i;
+        return +1;
+    }
 }
