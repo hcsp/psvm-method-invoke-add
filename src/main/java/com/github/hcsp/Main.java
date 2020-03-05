402a739a999a6323238d7ed4b2f4eb4c0becd0b9
硬核机器人 <49554844+hcsp-bot@users.noreply.github.com>
2020-03-05 13:34:29 +0800

    编写一个带参数的方法 (#178)
    编写一个带参数的方法 (#178)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -7,6 +7,10 @@ public class Main {
         System.out.println("3+1=" + addOne(3));
     }
 
+    private static String addOne(int i) {
+        return String.valueOf(i+1);
+    }
+
     // Create an addOne(int i) method here to increment an integer
     // 在这里编写一个addOne(int i)方法，对整数执行加1操作
 
