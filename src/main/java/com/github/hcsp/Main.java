711dcfd2560a65962bc3fe7f8d72804531a3487c
7952126 <15521608275@163.com>
2020-03-09 16:06:58 +0800

    addOne

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,12 +1,25 @@
 package com.github.hcsp;
 
+import java.sql.SQLOutput;
+
 public class Main {
+    public static int i;
     public static void main(String[] args) {
-        System.out.println("1+1=" + addOne(1));
-        System.out.println("2+1=" + addOne(2));
-        System.out.println("3+1=" + addOne(3));
+        addOne();
+        addOne();
+        addOne();
+
+       // System.out.println("1+1=" + addOne(1));
+        // System.out.println("2+1=" + addOne(2));
+       // System.out.println("3+1=" + addOne(3));
     }
 
+    public static void addOne() {
+        i = i + 1;
+
+
+
+    }
     // Create an addOne(int i) method here to increment an integer
     // 在这里编写一个addOne(int i)方法，对整数执行加1操作
 
