59c46dcc7729d911ae1dc0b545a35fbe46d27eb2
admin <1269583342@qq.com>
2020-05-16 19:56:51 +0800

    Create an addOne

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -6,6 +6,9 @@ public class Main {
         System.out.println("2+1=" + addOne(2));
         System.out.println("3+1=" + addOne(3));
     }
+    public static int addOne (int i) {
+        return i + 1;
+    }
 
     // Create an addOne(int i) method here to increment an integer
     // 在这里编写一个addOne(int i)方法，对整数执行加1操作
