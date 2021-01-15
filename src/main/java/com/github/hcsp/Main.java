01f11b414f2b7f194d1c9bc5e0e981ebf56c5d68
Apolo Du <dudueasy@126.com>
2021-01-16 02:16:50 +0800

    update (#255)
    update (#255)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -7,6 +7,10 @@ public class Main {
         System.out.println("3+1=" + addOne(3));
     }
 
+    public static int addOne(int args) {
+        return 1 + args;
+    }
+
     // Create an addOne(int i) method here to increment an integer
     // 在这里编写一个addOne(int i)方法，对整数执行加1操作
 
