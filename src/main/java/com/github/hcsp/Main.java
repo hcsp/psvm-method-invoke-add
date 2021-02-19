26c405dc088abc80e62a9710b36bcf1935b07051
antanghai <48671588+antanghai@users.noreply.github.com>
2021-02-19 09:30:12 +0800

    静态成员变量能够持续存在 (#260)
    静态成员变量能够持续存在 (#260)

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,6 +1,7 @@
 package com.github.hcsp;
 
 public class Main {
+    public static int a;
     public static void main(String[] args) {
         System.out.println("1+1=" + addOne(1));
         System.out.println("2+1=" + addOne(2));
@@ -9,5 +10,8 @@ public class Main {
 
     // Create an addOne(int i) method here to increment an integer
     // 在这里编写一个addOne(int i)方法，对整数执行加1操作
-
+    public static int addOne(int i) {
+        a = i + 1;
+        return a;
+    }
 }
