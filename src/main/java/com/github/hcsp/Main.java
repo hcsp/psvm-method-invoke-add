5b1133730349264ed6e917f82713b97d288c8586
YJL <2567843704@qq.com>
2021-09-19 16:17:30 +0800

    1

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,13 +1,19 @@
 package com.github.hcsp;
 
+import com.sun.org.apache.xpath.internal.objects.XNumber;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("1+1=" + addOne(1));
-        System.out.println("2+1=" + addOne(2));
-        System.out.println("3+1=" + addOne(3));
     }
+    public static int i ;
 
     // Create an addOne(int i) method here to increment an integer
     // 在这里编写一个addOne(int i)方法，对整数执行加1操作
+    public static void addOne() {
 
-}
+    }
+
+    {
+        i = i + 1;
+    }
+    }
