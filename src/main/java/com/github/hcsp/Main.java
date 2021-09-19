e60737262de9ae0deeff70030fb848d5fb9562af
YJL <2567843704@qq.com>
2021-09-19 16:47:11 +0800

    2

--- src/main/java/com/github/hcsp/Main.java
+++ src/main/java/com/github/hcsp/Main.java
@@ -1,11 +1,8 @@
 package com.github.hcsp;
-
-import com.sun.org.apache.xpath.internal.objects.XNumber;
-
 public class Main {
     public static void main(String[] args) {
     }
-    public static int i ;
+    public static int i;
 
     // Create an addOne(int i) method here to increment an integer
     // 在这里编写一个addOne(int i)方法，对整数执行加1操作
